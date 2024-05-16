d24457038b3779a7d78dd66aabbe53007bfc1871
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-05-16 19:29:47 +0530

    update:update in skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,21 +16,34 @@ const Skills = () => {
   console.log(data);
   return (
     <>
-      <div className="SkillHolder w-[100vw] bg-[#1E90FF]  p-5 ">
-        <h1 className="text-center text-5xl font-bold text-white">
-          Skills and <span className="text-[#feea33]">Abilities</span>
-        </h1>
-        <div className="flex space-x-10 flex-wrap mt-10">
+      <div className="SkillHolder w-12/12 bg-[#1E90FF]  p-5 flex flex-col justify-center items-center">
+        <div>
+          <div className="flex flex-col lg:flex-row lg:gap-x-5">
+            <div className="h-20 w-20 mt-[-1.45vmin] hidden lg:block">
+              <img
+                src="./images/computer.webp"
+                className="max-w-full max-h-full object-contain"
+              ></img>
+            </div>
+            <h1 className="text-center text-4xl lg:text-5xl font-bold text-white">
+              Skills and <span className="text-[#feea33]">Abilities</span>
+            </h1>
+          </div>
+          <p className="text-center text-white text-lg">
+            ( Every skill you acquire doubles your odds of success )
+          </p>
+        </div>
+        <div className="flex gap-y-5 gap-x-5 flex-wrap justify-center items-center mt-10">
           {data.map((val) => (
             <>
-              <div className="cardHolder  h-52 w-52 bg-[#052340] shadow-xl flex flex-col  justify-center text-white font-extrabold items-center font-sans">
-                <div className="imgHolder h-24 w-24 ">
+              <div className="cardHolder p-5 w-40 lg:w-48 bg-[#0c355b] shadow-xl flex flex-col  justify-center text-white font-medium items-center font-sans">
+                <div className="imgHolder h-20 w-20">
                   <img
                     src={val.image}
                     className="max-w-full max-h-full object-contain"
                   ></img>
                 </div>
-                <p className="text-center text-2xl mt-5">{val.name}</p>
+                <p className="text-center text-xl mt-5">{val.name}</p>
               </div>
             </>
           ))}
